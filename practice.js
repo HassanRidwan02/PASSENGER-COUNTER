f7924cd78e7de6acd7e6ea086aae4edb9e029ea1
HassanRidwan02 <157763165+HassanRidwan02@users.noreply.github.com>
2024-01-26 00:08:43 +0100

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,65 @@
+let count = 0 
+let countEL = document.getElementById("count-el")
+
+let saveEl = document.getElementById("save-el")
+
+function increment(){
+   count++
+   countEL.textContent = count
+}
+
+function decrement(){
+   if (count > 0){
+      count--
+      countEL.textContent = count
+   }
+   
+}
+
+function save(){
+   saveEl.textContent += " " + count + " - "
+   count = 0
+   countEL.textContent = 0
+
+}
+
+
+let num1 = randomnumber()
+let num2 = randomnumber()
+
+let num1El = document.getElementById("num1-el")
+let num2El = document.getElementById("num2-el")
+
+num1El.textContent = num1
+num2El.textContent = num2
+
+let resultEL = document.getElementById("result-el")
+
+function add(){
+   let sum = num1 + num2
+   resultEL.textContent = "Sum: " + sum
+}
+
+function sub(){
+   let diff = num1 - num2
+   resultEL.textContent = "Difference: " + diff
+}
+
+function div(){
+   let div = num1 / num2
+   resultEL.textContent = "Division: " + div
+}
+
+function mul(){
+   let prod = num1 * num2
+   resultEL.textContent = "Product: " + prod
+}
+
+
+function randomnumber(){
+   return Math.floor(Math.random() * 10)
+}
+
+function refresh(){
+   window.location.reload("Refresh")
+}
